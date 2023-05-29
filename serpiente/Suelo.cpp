588a7b2fff64fad29ac65c279630f3df443f4bc3
anton <antonio.farina@rai.usc.es>
2023-05-29 12:32:42 +0200

    clase suelo

---
+++ serpiente/Suelo.cpp
@@ -0,0 +1,56 @@
+#include "suelo.h"
+
+
+Suelo::Suelo(GLuint VAO, GLuint textura1, GLuint textura2) {
+	this->limite = LIMITE;
+	this->escala = ESCALA;
+	this->VAO = VAO;
+	this->textura1 = textura1;
+	this->textura2 = textura2;
+}
+
+void Suelo::dibujar(GLuint shader) {
+	//creamos las matrices del modelo
+	glm::mat4 model;
+	//la busco en el shader
+	unsigned int modelLoc = glGetUniformLocation(shader, "model");
+	glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+	int i, j;
+	for (i = -limite; i <= limite; i++) {
+		for (j = -limite; j <= limite; j++) {
+			//Calculo la matriz
+			model = glm::mat4(); //identity matrix
+			//trasladamos para dibujar cada cuadrado
+			model = glm::translate(model, glm::vec3(i * escala, j * escala, 0.0f));
+			model = glm::scale(model, glm::vec3(escala, escala, 1));
+			if ((i + j) % 2) {
+				glBindTexture(GL_TEXTURE_2D, textura1);
+			}
+			else {
+				glBindTexture(GL_TEXTURE_2D, textura2);
+			}
+			//La cargo
+			glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+			//dibujamos el cuadrado
+			glBindVertexArray(VAO);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+		}
+	}
+}
+
+int Suelo::getLimite(){
+	return this->limite;
+}
+
+void Suelo::setLimite(int limite){
+	this->limite = limite;
+}
+
+float Suelo::getEscala(){
+	return this->escala;
+}
+
+void Suelo::setEscala(float escala){
+	this->escala = escala;
+}
---
+++ serpiente/suelo.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "glad.h"
+
+#define LIMITE 10
+#define ESCALA 1
+class Suelo {
+private:
+    int limite;
+    float escala;
+    GLuint textura1, textura2;
+    GLuint VAO;
+public:
+    Suelo(GLuint VAO, GLuint textura1, GLuint textura2);
+    void dibujar(GLuint shader);
+    int getLimite();
+    void setLimite(int limite);
+    float getEscala();
+    void setEscala(float escala);
+};
